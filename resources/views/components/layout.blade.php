0048d7b800f4ef02cbcfea04b4f5ffe41efd76fc
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-08-27 10:44:11 -0500

    created new laravel project

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pixel Positions</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+<body>
+    <div>
+        <nav class="flex justify-between">
+            <div>
+                <a href="/"><img src="{{ Vite::asset('resources/images/logo.svg') }}" alt=""></a>
+            </div>
+            <div>
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
+            <div>
+                <a href="#">Post a Job</a>
+            </div>
+        </nav>
+
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+    
+</body>
+</html>
