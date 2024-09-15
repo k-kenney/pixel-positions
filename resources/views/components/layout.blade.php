8a1351fa922f2ac12d6cb545f7e3a2e75a31e2ec
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-09-15 18:02:44 -0500

    set up project and architecture

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,13 +6,13 @@
     <title>Pixel Positions</title>
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
-<body>
-    <div>
-        <nav class="flex justify-between">
+<body class="bg-black text-white">
+    <div class="px-10">
+        <nav class="flex justify-between items-center py-4 border-b border-white/10">
             <div>
                 <a href="/"><img src="{{ Vite::asset('resources/images/logo.svg') }}" alt=""></a>
             </div>
-            <div>
+            <div class="space-x-6 font-bold">
                 <a href="#">Jobs</a>
                 <a href="#">Careers</a>
                 <a href="#">Salaries</a>
