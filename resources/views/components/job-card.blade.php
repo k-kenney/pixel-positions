5b909522c3b8d86f95963e8745a6ebc6678a7652
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-09-23 19:01:37 -0500

    styled tags and added header

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,16 +1,16 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-600 group transition-colors duration-300">
+<x-panel class="flex flex-col">
     <div class="self-start text-sm">Laracasts</div>
-    <div class="py-8">
+    <div class="py-8 text-center">
         <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">Video Producer</h3>
         <p class="text-sm mt-4">Full Time - From $60,000</p>
     </div>
     <div class="flex justify-between items-center my-auto">
         <div>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
+            <x-tag size="small">Backend</x-tag>
+            <x-tag size="small">Frontend</x-tag>
+            <x-tag size="small">Manager</x-tag>
         </div>
 
         <x-employer-logo :width="42" />
     </div>
-</div>
+</x-panel>
